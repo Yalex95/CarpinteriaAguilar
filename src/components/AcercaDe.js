391acedf023c52de83abaa841cd9628f7111bbc0
Yeris Aguilar <yerisaguilarg@gmail.com>
2022-02-11 14:07:24 -0600

    hmtl update and css

--- src/components/AcercaDe.js
+++ src/components/AcercaDe.js
@@ -12,7 +12,7 @@ export const AcercaDe = () => {
       <div className="alert alert-info text-center">Loading...</div>
       
     ):(
-      <div className='container-fluid mt-5 mb-5' id='acerca'>
+      <div className='container-fluid mt-5 mb-5 ' id='acerca'>
       <h1 className='bodoni-txt-title mb-3 mt-3'>{titulo}</h1>
       <h2 className='bodoni-text'>
       <div dangerouslySetInnerHTML={{__html: desc}} />
